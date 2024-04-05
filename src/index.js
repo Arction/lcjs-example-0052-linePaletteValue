7be050c97d1718cb8080e8ffe4e7a69feff14e95
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:10:36 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ const {
     Themes,
 } = lcjs
 
-const chart = lightningChart()
+const chart = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
     .ChartXY({
         theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     })
